a3c2f452dd21cc0f764407776d5423dace46e8f4
Georgios Pseiridis Pseiras <g.pseiridis@gmail.com>
2020-12-30 00:03:12 +0100

    Rules MEM50CPP MEM51CPP added

--- CPPRules/MemoryManagement.h
+++ CPPRules/MemoryManagement.h
@@ -22,7 +22,7 @@ void nonCompliantExample1(int inputNumber, const char* inputStr)
              * is accessed after it has been implicitly deallocated
              * by the object's destructor.*/
             std::unique_ptr<char[]> buff(new char[BufferSize]);
-            std::memset(buff.get(), 0, BufferSize); //self note: you could delete this and still won't work
+            std::memset(buff.get(), 0, BufferSize);  // self note: you could delete this and still won't work
             // ... do stuff
             someStr = std::strncpy(buff.get(), inputStr, BufferSize - 1);
         }
@@ -65,19 +65,17 @@ int compliantExample1(int inputNumber, const char* inputStr)
     std::cout << "*Compliant* What will someStr print? someStr =  " << someStr << std::endl;
 }
 
-
 void nonCompliantExample2(int inputNumber, const char* inputStr)
 {
     constexpr unsigned int BUFFER_SIZE = 32;
     const char* someStr = "";
-    if(inputNumber >0)
+    if (inputNumber > 0)
     {
         std::unique_ptr<char[]> buff(new char[BUFFER_SIZE]);
         //... do stuff
         someStr = std::strncpy(buff.get(), inputStr, BUFFER_SIZE - 1);
     }
     std::cout << "*Non Compliant2 * What will someStr print? someStr =  " << someStr << std::endl;
-
 }
 
 void compliantExample2(int inputNumber, const char* inputStr)
@@ -86,7 +84,7 @@ void compliantExample2(int inputNumber, const char* inputStr)
     const char* someStr = "";
     std::unique_ptr<char[]> buff(new char[BUFFER_SIZE]);
 
-    if(inputNumber >0)
+    if (inputNumber > 0)
     {
         buff.reset(new char[BUFFER_SIZE]);
         //... do stuff
@@ -96,3 +94,98 @@ void compliantExample2(int inputNumber, const char* inputStr)
 }
 
 }  // namespace MEM50CPP
+
+/*MEM51-CPP. Properly deallocate dynamically allocated resources */
+namespace MEM51CPP
+{
+/*
+Comments: Even better don’t use ‘new’ and ‘delete’ at all.
+Adhere to cpp core guidelines, avoiding explicitly calling new+delete or calling malloc, free.
+Try to use smart pointers or STL containers.
+Sources from c++ core guidelines:
+http://isocpp.github.io/CppCoreGuidelines/CppCoreGuidelines#Rr-newdelete
+http://isocpp.github.io/CppCoreGuidelines/CppCoreGuidelines#Rr-mallocfree
+ */
+
+void nonCompliantExample1()
+{
+    int* array = new int[10];
+    // ... do stuff
+    delete array;
+}
+
+void compliantExample1()
+{
+    int* array = new int[10];
+    // ... do stuff
+    delete[] array;
+}
+
+void nonCompliantExample2()
+{
+    int* i = static_cast<int*>(std::malloc(sizeof(int)));
+    // ... do stuff
+    delete i;
+}
+
+void compliantExample2()
+{
+    int* i = static_cast<int*>(std::malloc(sizeof(int)));
+    // ... do stuff
+    std::free(i);
+    std::cout << " ***** \n";
+}
+
+struct MyStruct
+{
+    ~MyStruct();
+};
+
+void nonCompliantExample3()
+{
+    MyStruct* s = new MyStruct();
+    // ...
+    std::free(s);
+    std::cout << "called 'free' instead of 'delete' after dynamically allocating with 'new'";
+}
+
+void nonCompliantExample4()
+{
+    std::cout << "a std::unique_ptr is declared to hold a pointer to an object, \n";
+    std::cout << "but is direct-initialized with an array of objects. \n";
+    std::cout << "When the std::unique_ptr is destroyed, its default deleter calls delete instead of delete[]. \n";
+    std::cout << "resulting in undefined behaviour. \n";
+
+    std::unique_ptr<MyStruct> mySmartPtr{new MyStruct[10]};
+
+    std::cout << " *********** Non Compliant Example with Shared Pointer ***********\n";
+    /*
+     In this noncompliant code example, a std::shared_ptr is declared to hold a pointer to an object, but is
+     direct-initialized with an array of objects. As with std::unique_ptr, when the std::shared_ptr is destroyed, its
+     default deleter calls delete instead of delete[], resulting in undefined behavior.
+    */
+    std::shared_ptr<MyStruct> mySharedPtr{new MyStruct[10]};
+}
+
+void compliantExample4()
+{
+    //  instead of a pointer to
+    // an object. Additionally, std::make_unique() is used to initialize the smart pointer.
+
+    std::cout << "In this compliant solution, the std::unique_ptr is declared to hold an array of objects. \n";
+    std::cout << "instead of a pointer to an object \n";
+    std::cout << "Additionally, std::make_unique() is used to initialize the smart pointer. (C++14) \n";
+
+    //     std::unique_ptr<MyStruct[]> mySmartPtr = std::make_unique<MyStruct[]>(10); //C++ 14
+
+    std::cout << " *********** Compliant Example with Shared Pointer ***********\n";
+    /* Unlike the compliant solution for std::unique_ptr,
+     * where std::make_unique() is called to create a unique pointer
+     * to an array, it is ill-formed to call std::make_shared() with an array type.
+     * Instead, this compliant solution
+     * manually specifies a custom deleter for the shared pointer type,
+     * ensuring that the underlying array is properly deleted. */
+    std::shared_ptr<MyStruct> mySharedPtr{new MyStruct[10], [](const MyStruct* ptr) { delete[] ptr; }};
+}
+
+}  // namespace MEM51CPP
--- main.cpp
+++ main.cpp
@@ -6,15 +6,14 @@
 #include <iostream>
 #include <vector>
 
-
 int main()
 {
     /*Rule 01. Declarations and Initialization (DCL) */
-    DCL50CPP::nonCompliantAddition(13, 37, 1,42,"hello",-4);
-    DCL50CPP::compliantAddition(1,2,3,0,1);
+    DCL50CPP::nonCompliantAddition(13, 37, 1, 42, "hello", -4);
+    DCL50CPP::compliantAddition(1, 2, 3, 0, 1);
 
     /*Rule 04. Containers (CTR) */
-    std::vector<int> myVector = {1,2,3,4,5,6,7,8};
+    std::vector<int> myVector = {1, 2, 3, 4, 5, 6, 7, 8};
     CTR52CPP::compliantExample3();
     CTR55CPP::compliantExample1(myVector);
 
@@ -23,13 +22,13 @@ int main()
     /*Rule 06. Memory Management (MEM) */
     MEM50CPP::nonCompliantExample1(42, "hello");
     MEM50CPP::nonCompliantExample2(20, "hello people");
-
     MEM50CPP::compliantExample1(42, "hello");
     MEM50CPP::compliantExample2(42, "hello people");
 
-
-
-
-
-
+    MEM51CPP::nonCompliantExample1();
+    MEM51CPP::nonCompliantExample2();
+    MEM51CPP::nonCompliantExample3();
+    MEM51CPP::nonCompliantExample4();
+    MEM51CPP::compliantExample1();
+    MEM51CPP::compliantExample2();
 }
